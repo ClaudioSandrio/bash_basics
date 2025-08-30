9d59a9e629a6066a3cf03ba7f8cfd29eafca17d5
Claudio Sandrio <carrelsandrio8@gmail.com>
2025-08-30 12:55:35 -0400

    Add custom bash script with welcome message

---
+++ 01-my_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+#!/usr/bin/env bash
+echo "Welcome! This script was customized by Claudio Sandrio."
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
